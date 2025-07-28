af925e09ba66ea98cd3a58c53edaad97ee39b94d
BravestCheetah <bravestcheetah@gmail.com>
2025-07-28 20:21:10 +0200

    finished the link manager add_link function

--- src/dirlink/link_manager.py
+++ src/dirlink/link_manager.py
@@ -1,8 +1,27 @@
 from src.dirlink.path_data import PathData
+from src.dirlink.log import log
 
 class LinkManager():
     def __init__(self):
         pass
 
-    def add_link(self, link: str, params: list = []):
-        pass
+    # Returns Boolean, true: exit the process (error occured), false: keep going
+    def add_link(self, link: str) -> bool:
+        data_path = PathData.data_path()
+
+        with open(data_path / "links.dlink", "a+") as f:
+
+            data = f.read()
+            data = data.split("\n")
+
+            if link in data:
+                log("err", "Link Already Exists. Error code: 1")
+                f.close()
+                return True
+            
+            f.write(link)
+            f.close()
+        
+        log("info", "Successfully Saved Link")
+        return False
+            
--- src/dirlink/log.py
+++ src/dirlink/log.py
@@ -3,11 +3,12 @@ from termcolor import colored
 log_types = {
     "info": colored("[ INFO ]", "green"),
     "warn": colored("[ WARNING ]", "yellow"),
-    "err": colored("[ ERROR ]", "red")
+    "err": colored("[ ERROR ]", "red"),
+    "succ": colored("[ SUCCESS ]", "light_green")
 }
 
 colored()
 
 def log(type, message):
     # Example: [ DIRLINK ] [ INFO ] Sucessfully Saved Link
-    print(f"{colored(" [ DIRLINK ] ")}{log_types[type]}{message}")
+    print(f"{colored(" [ DIRLINK ] ", "blue")}{log_types[type]}{message}")
