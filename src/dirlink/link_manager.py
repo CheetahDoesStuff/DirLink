f87cd87a112d2f18d45378ff1b76d47f33809a50
BravestCheetah <bravestcheetah@gmail.com>
2025-07-28 20:44:04 +0200

    added a function to verify if a link exists or not

--- src/dirlink/link_manager.py
+++ src/dirlink/link_manager.py
@@ -5,25 +5,22 @@ class LinkManager():
     def __init__(self):
         pass
 
+
     # Returns Boolean, true: exit the process (error occured), false: keep going
     def add_link(self, link: str) -> bool:
         data_path = PathData.data_file()
 
-        with open(data_path, "a+") as f:
+        if verify_link(link):
+            return True
 
-            data = f.readlines()
-
-            if link in data:
-                log("err", "Link Already Exists. Error code: 1")
-                f.close()
-                return True
-            
+        with open(data_path, "a") as f:
             f.write(link)
             f.close()
         
         log("info", "Successfully Saved Link")
         return False
     
+
     # Returns Boolean, true: exit the process (error occured), false: keep going
     def rm_link(self, link: str) -> bool:
         data_path = PathData.data_file()
@@ -34,3 +31,17 @@ class LinkManager():
             for link in data:
                 if link.strip("\n") != link:
                     f.write(link)
+    
+
+    # Returns Boolean, true: link exists, false: link doesnt exist
+    def verify_link(self, link: str) -> bool:
+        data_path = PathData.data_file()
+
+        with open(data_path, "r") as f:
+            data = f.readlines()
+
+            for loop_link in data:
+                if loop_link.strip("\n") == link:
+                    return True
+            
+            return False
