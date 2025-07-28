a09277b51c6a63b3ed83e80b536a3ccf97c1b66f
BravestCheetah <bravestcheetah@gmail.com>
2025-07-28 19:58:21 +0200

    Added Log Function For Quick And Nice Looking Log Messages

--- src/dirlink/log.py
+++ src/dirlink/log.py
@@ -1,6 +1,13 @@
 from termcolor import colored
 
+log_types = {
+    "info": colored("[ INFO ]", "green"),
+    "warn": colored("[ WARNING ]", "yellow"),
+    "err": colored("[ ERROR ]", "red")
+}
 
+colored()
 
-def log(service, message):
-    pass
+def log(type, message):
+    # Example: [ DIRLINK ] [ INFO ] Sucessfully Saved Link
+    print(f"{colored(" [ DIRLINK ] ")}{log_types[type]}{message}")
