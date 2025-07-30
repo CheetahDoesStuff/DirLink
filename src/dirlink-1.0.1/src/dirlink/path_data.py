3a93afc10c00979618dea54b951c55ef39d8f292
BravestCheetah <bravestcheetah@gmail.com>
2025-07-30 19:54:43 +0200

    removed veresion specific produces and added to gitingore

--- src/dirlink-1.0.1/src/dirlink/path_data.py
+++
@@ -1,7 +0,0 @@
-from platformdirs import user_data_dir
-from pathlib import Path
-
-class PathData:
-    
-    def data_file():
-        return Path(user_data_dir("DirLink", "BravestCheetah")) / "links.dl-dat"
